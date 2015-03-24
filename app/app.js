d00720e270a4d732bb3cd7dbb1f6d622b3c68369
Hanaa <hanaa.gaafar@gmail.com>
2015-03-24 12:01:10 +0200

    First commit

---
+++ app/app.js
@@ -0,0 +1,10 @@
+
+// Create App
+angular.module('WissenkartenApp', ['ui.router','ui.bootstrap']);
+
+// Get App
+var app = angular.module('WissenkartenApp');
+app.run(['$rootScope', function($rootScope) {
+	
+
+}]);
